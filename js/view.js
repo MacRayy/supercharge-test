aa2b962200b25d95afa0e39614e576c1c760eae4
Zsolt Makrai <zsolt.makrai@blackswan.com>
2018-04-30 15:53:32 +0200

    feat(cards): shuffles the cards before rendering

--- js/view.js
+++ js/view.js
@@ -9,17 +9,28 @@ const UIController = (function () {
 	const setupGameBoard = () => {
 		const selector = document.querySelector(DOMStrings.selectCardNum)
 		const deckSize = selector.options[selector.selectedIndex].value
+
 		renderCards(deckSize)
 	}
 
 	const renderCards = (size) => {
 		const gameBoard = document.getElementById(DOMStrings.gameBoard)
+		const cards = []
+
 		clearGameBoard(gameBoard)
+
 		for (let i = 0; i < size; i++) {
 			const card = document.createElement('div')
 			card.className = 'card card-back'
-			gameBoard.appendChild(card)
+			cards.push(card)
 		}
+
+		shuffle(cards)
+
+		cards.forEach(c => {
+			gameBoard.appendChild(c)
+		})
+
 		addEventListenersToCards()
 	}
 
@@ -28,12 +39,28 @@ const UIController = (function () {
 	const addEventListenersToCards = () => {
 		const cards = Array.from(document.getElementsByClassName('card'))
 		cards.forEach(c => c.addEventListener('click', changeCardStatus))
+		shuffle(cards)
 	}
 
 	const changeCardStatus = function () {
 		this.classList.toggle('card-back')
 		this.classList.toggle('card-front')
 		this.classList.toggle('card-disabled')
+	}
+
+	const shuffle = (array) => {
+		let currentIndex = array.length
+		let temporaryValue = 0
+		let randomIndex = 0
+
+		while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex)
+			currentIndex -= 1
+			temporaryValue = array[currentIndex]
+			array[currentIndex] = array[randomIndex]
+			array[randomIndex] = temporaryValue
+		}
+		return array
 	}
 
 	return {
